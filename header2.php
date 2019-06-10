997e1ae03fec887592b6a547285d3319d587385f
unknown <=>
2019-06-10 11:40:59 -0700

    add one device from ajax no complate top in accoridan

---
+++ header2.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title> مؤسســــة عالــــم الاكتشـــاف </title>
+
+    <!-- Bootstrap -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
+    <link href="css/fontawesome-all.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+     <link href="css/rtl.css" rel="stylesheet">
+
+    <link href="css/media.css" rel="stylesheet">
+
+<!-- 8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+
+
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  
+  <!--start top head -->
+  <div class="top-head">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-xs-12 col-sm-4">
+          <div class="logo-div">
+            <div class="logo">
+              <img class="img-responsive" src="images/logo.png" alt="logo">
+            </div>
+            <div class="lang">
+              <a href="index.php"><img src="images/ar.jpg" alt="ar"></a>
+              <a href="en/index.php"><img src="images/en.jpg" alt="en"></a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-5">
+          <div class="company">
+            <h2>مؤسســــة عالــــم الاكتشـــاف </h2>
+             <p> أجهزة كشف الذهب والكنوز والمياه الجوفية </p>
+          </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-3">
+          <div class="phone">
+            <img class="img-responsive" src="images/phone.png" alt="phone">
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+ <!--end top head -->
+<!--start navbar -->
+  <nav class="navbar navbar-default">
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="index.php">الرئيسية <span class="sr-only">(current)</span></a></li>
+        <li><a href="aboutus.php"> من نحن</a></li>
+       <li><a href="categories.php">اجهزتنا </a></li>
+         <li><a href="buy.php">الشراء </a></li>
+          <li><a href="contactus.php"> أتصل بنا </a></li>
+      </ul>
+      
+    </div><!-- /.navbar-collapse --> 
+  </div><!-- /.container-fluid -->
+</nav>
+<!--end navbar -->
+
+
+
+<!--start containt -->
+  <div class="container-fluid">
+    <div class="slinks">
+      <a target="_blank" href="#"><i class="fab fa-facebook-square"></i> </a>
+      <a target="_blank" href="#"><i class="fab fa-twitter-square"></i> </a>
+      <a target="_blank" href="#"><i class="fab fa-google-plus-square"></i></a>
+      <a target="_blank" href="#"><i class="fab fa-youtube-square"></i> </a>
+    </div>
+
+
+
+
+
